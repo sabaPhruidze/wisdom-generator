1ea4e496e5e284e137e1f10a68bb381824ec245f
Saba Phruidze <sabaphruidze01@gmail.com>
2025-10-23 13:43:24 +0400

    added 2 images, imported themedtext and themedview in order to adjust darkmode and lightmode of the mobile, also adjusted activity indicator amd learned how to adjust size of the image to the mobile screen

---
+++ components/adviceCard.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { ActivityIndicator, Dimensions, Image, StyleSheet } from "react-native";
+import { ThemedText } from "./themed-text";
+import { ThemedView } from "./themed-view";
+
+interface AdviceCardProps {
+  advice: string;
+  isLoading: boolean;
+}
+
+const AdviceCard = (props: AdviceCardProps) => {
+  const { isLoading, advice } = props;
+  return (
+    <ThemedView style={styles.container}>
+      <Image
+        source={require("../assets/images/dark.png")}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <ThemedText type="title">{advice}</ThemedText>
+      )}
+    </ThemedView>
+  );
+};
+
+export default AdviceCard;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16,
+  },
+  image: {
+    width: Dimensions.get("window").width,
+    aspectRatio: 1,
+  },
+  text: {
+    color: "#ffffff",
+    fontSize: 16,
+  },
+});
